b1aa5ccd5cb94a6b404b8adbcf5aa5d5751f030c
sukhbirkc <97969645+sukhbirkc@users.noreply.github.com>
2022-03-25 11:09:02 -0400

    Add files via upload

---
+++ C02_sukhbir kaur/js/update-array.js
@@ -0,0 +1,13 @@
+//create the array
+var colors = ['white', 
+	          'black', 
+	          'custom'];
+
+// Update the third item in the array
+colors[2] = 'beige';
+
+//Get the element with an id of colors
+var el = document.getElementById('colors');
+
+//Replace with third item from the array 
+el.textContent = colors(2);
